538e438cc78739f922f337aefcc55f0c66e8cf19
Eg <mail@olgerdt.ru>
2020-10-08 01:53:25 +0300

    корректировка

--- 5-2.py
+++ 5-2.py
@@ -2,7 +2,7 @@
 # подсчет количества строк, количества слов в каждой строке.
 # ****************
 # 5-2.txt
-# str1
+# str1 str1_2
 # str2
 # str3
 # str4
@@ -12,5 +12,6 @@ with open("5-2.txt", encoding="utf-8") as f_obj:
     i = 0
     for line in f_obj:
         i += 1
+        print(len([x for x in line.split()]))
 
 print(i)
